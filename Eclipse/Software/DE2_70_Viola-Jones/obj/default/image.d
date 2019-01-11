f488512f0ba0963a417df7d828ebc8cbb878eff9
TakeshiYoshikawa <takeshi-y@hotmail.com>
2019-01-11 15:19:01 -0300

    compiled files

---
+++ Eclipse/Software/DE2_70_Viola-Jones/obj/default/image.d
@@ -0,0 +1,5 @@
+obj/default/image.o: image.c image.h stdio-wrapper.h
+
+image.h:
+
+stdio-wrapper.h:
---
+++ Eclipse/Software/DE2_70_Viola-Jones/obj/default/rectangles.d
@@ -0,0 +1,7 @@
+obj/default/rectangles.o: rectangles.cpp haar.h image.h stdio-wrapper.h
+
+haar.h:
+
+image.h:
+
+stdio-wrapper.h:
